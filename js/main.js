e00fb1fec42ffc62cbd0777896031f01c83cabca
Nicholasb9592 <143469507+Nicholasb9592@users.noreply.github.com>
2025-10-26 17:00:44 -0500

    Add function to fetch Arkansas movies from TMDB

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+const apiKey = "YOUR_TMDB_API_KEY";
+const apiBase = "https://api.themoviedb.org/3";
 
+async function getArkansasMovies() {
+  const response = await fetch(`${apiBase}/search/movie?query=Arkansas&api_key=${apiKey}`);
+  const data = await response.json();
+  console.log(data.results);
+}
+
+getArkansasMovies();
