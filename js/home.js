a7ecbd16b8656a79713c301bceeba65df1cb9fe2
Nicholasb9592 <143469507+Nicholasb9592@users.noreply.github.com>
2025-12-01 14:49:10 -0600

    Refactor movie and actor loading functions

--- js/home.js
+++ js/home.js
@@ -3,6 +3,7 @@ const arkansasMovies = [
   "Sling Blade",
   "True Grit",
   "Arkansas",
+  "A Face in the Crowd",
   "The Quick and the Dead"
 ];
 
@@ -13,65 +14,61 @@ const arkansasActors = [
   "Joey Lauren Adams"
 ];
 
-function pickRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
 }
 
-/* Featured Movie */
 async function loadFeaturedMovie() {
-  const container = document.getElementById("featured-movie");
-  container.innerHTML = "<p>Loading...</p>";
+  const box = document.getElementById("featured-movie");
 
   try {
     const title = pickRandom(arkansasMovies);
-    const found = await searchMovieRaw(title);
-    if (!found) throw new Error("No movie found!");
+    const movie = await searchMovieRaw(title);
+    if (!movie) throw new Error("Movie not found");
+    const details = await getMovieDetails(movie.id);
 
-    const details = await getMovieDetails(found.id);
     const poster = tmdbImage(details.poster_path, "w342");
 
-    container.innerHTML = `
-      <div class="featured-box" style="cursor:pointer;">
+    box.innerHTML = `
+      <div class="featured-box">
         <img src="${poster}" alt="${details.title}">
         <h4>${details.title}</h4>
         <p>${details.overview.slice(0, 120)}...</p>
       </div>
     `;
 
-    container.onclick = () => openMovieDetail(details);
+    box.onclick = () => openMovieDetail(details);
 
   } catch (err) {
-    container.innerHTML = "<p>Unable to load featured movie.</p>";
     console.error(err);
+    box.innerHTML = "<p>Unable to load movie</p>";
   }
 }
 
-/* Featured Actor */
 async function loadFeaturedActor() {
-  const container = document.getElementById("featured-actor");
-  container.innerHTML = "<p>Loading...</p>";
+  const box = document.getElementById("featured-actor");
 
   try {
     const name = pickRandom(arkansasActors);
-    const found = await searchPersonRaw(name);
-    if (!found) throw new Error("No actor found!");
+    const person = await searchPersonRaw(name);
+    if (!person) throw new Error("Actor not found");
+    const details = await getPersonDetails(person.id);
 
-    const details = await getPersonDetails(found.id);
-    const photo = tmdbImage(details.profile_path, "w300");
+    const img = tmdbImage(details.profile_path);
 
-    container.innerHTML = `
-      <div class="featured-box" style="cursor:pointer;">
-        <img src="${photo}" alt="${details.name}">
+    box.innerHTML = `
+      <div class="featured-box">
+        <img src="${img}" alt="${details.name}">
         <h4>${details.name}</h4>
-        <p>${details.biography ? details.biography.slice(0,120) + "..." : "Biography unavailable."}</p>
+        <p>${details.biography ? details.biography.slice(0, 120) + "..." : "Biography unavailable."}</p>
       </div>
     `;
 
-    container.onclick = () => openActorDetail(details);
+    box.onclick = () => openActorDetail(details);
 
   } catch (err) {
-    container.innerHTML = "<p>Unable to load featured actor.</p>";
     console.error(err);
+    box.innerHTML = "<p>Unable to load actor</p>";
   }
 }
 
